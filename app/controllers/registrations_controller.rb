6f0d2303b56c189bae7128dd8fe52d402fc2f464
Ricardo Rojas <reddrum7@gmail.com>
2015-09-25 17:01:37 -0500

    User update with password or not

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,14 +3,31 @@ class RegistrationsController < Devise::RegistrationsController
     new_params = params.require(:user).permit(:email,
     :username, :current_password, :password,
     :password_confirmation)
+    change_password = true
+
+    if params[:user][:password].blank?
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+      new_params = params.require(:user).permit(:email, :username)
+      change_password = false
+    end
 
     @user = User.find(current_user.id)
-    if @user.update_with_password(new_params)
+    is_valid = false
+
+    if change_password
+      is_valid = @user.update_with_password(new_params)
+    else
+      @user.update_without_password(new_params)
+    end
+
+    if is_valid
       set_flash_message :notice, :updated
       sign_in @user, :bypass => true
       redirect_to after_update_path_for(@user)
     else
       render "edit"
     end
+
   end
 end
